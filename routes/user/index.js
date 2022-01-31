0e6d2626d8fcef0bab2d2571d79d330dc2d505a7
Sebastián Castañeda <sebastiansnipez@gmail.com>
2022-01-31 15:14:07 -0500

    more refactor

--- routes/user/index.js
+++ routes/user/index.js
@@ -1,16 +1,11 @@
 const express = require("express")
-const books = require("./books")
-const profile = require("./profile")
-const feed = require("./feed")
-const notifications = require("./notifications")
-const requests = require("./requests")
 
 const router = express.Router()
-router.use("/books", books)
-router.use("/profile", profile)
-router.use("/feed", feed)
-router.use("/notifications", notifications)
-router.use("/requests", requests)
+router.use("/books", require("./books"))
+router.use("/profile", require("./profile"))
+router.use("/feed", require("./feed"))
+router.use("/notifications", require("./notifications"))
+router.use("/requests", require("./requests"))
 router.use("/contacts", require("./contacts"))
 
 module.exports = router
